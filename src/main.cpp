318abb0c561c47a075802e021d439582fda5d45d
Jon Durrant <jon@jondurrant.com>
2023-04-20 11:40:14 +0100

    Celebrate on Paper

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+/**
+ * Jon Durrant.
+ *
+ * Badger YouTube 500 Celebration
+ */
+
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include "pico/time.h"
+#include "pico/platform.h"
+
+#include "common/pimoroni_common.hpp"
+#include "badger2040.hpp"
+
+#include "slides.h"
+
+#define DELAY 5000
+#define SLIDES 8
+
+using namespace pimoroni;
+
+Badger2040 badger;
+
+
+const uint8_t * images[]= {
+	Slide1,
+	Slide2,
+	Slide3,
+	Slide4,
+	Slide5,
+	Slide6,
+	Slide7,
+	Slide8
+};
+
+
+int main() {
+	int i = 0;
+
+	stdio_init_all();
+
+	sleep_ms(2000);
+	printf("GO\n");
+
+	badger.init();
+
+	for (;;){
+		for ( i = 0; i < SLIDES; i++){
+			badger.led(0xFF);
+			badger.image(images[i]);
+
+			badger.update();
+			while (badger.is_busy()) {
+				sleep_ms(10);
+			}
+			badger.led(0);
+			sleep_ms(DELAY);
+		}
+	}
+
+
+
+	badger.halt();
+
+}
